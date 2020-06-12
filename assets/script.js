5bf45d82ff0ec5a2bc0c7712986f70efb8fbb31e
Conner Jarrell <connerjarrell07@gmail.com>
2020-06-11 21:13:51 -0400

    add Clear button and function

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,7 @@
-// DOM Ready
 $(document).ready(function() {
 var submit = document.getElementsByTagName('button');
     
-// Moment
 var currentDay = moment().format('MMMM DD, YYYY');
-// add current day to its html id
 $('#currentDay').text(currentDay);
     
 var hoursArray = ['9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm'];
@@ -14,44 +11,63 @@ function timeblockCreation(){
         var newRow = $('<div>')
         $(newRow).attr({class: 'row', id: 'row' + (i + 1) });
         $('.container').append(newRow);
-        // left side time column
+        
         var hour = $("<div>");
         $(hour).attr({ class: "hour col-1", id: 9 + i }).text(hoursArray[i]);
-        // text ares for client input
+        
         var description = $("<div>");
         $(description).attr("class", "description col-10");
         var descriptionRow = $("<div>").attr({ class: "row", id: "description" + (i + 1) });
         $(description).append(descriptionRow);
-        //Cells with saved items
+        
         var textArea = $("<textarea>").attr({ class: "col-12", id: "textareabutton" + (i + 1) }).text(localStorage.getItem("myEventbutton" + (i + 1)));
         $(descriptionRow).append(textArea);
-        //Save button
+    
         var saveBtn = $("<button>");
-        $(saveBtn).attr({ class: "saveBtn col-1 ", id: "button" + (i + 1) }).text("Save to schedule");
+        $(saveBtn).attr({ class: "saveBtn col-1", id: "button" + (i + 1) }).text("Save Appt");
         $(newRow).append(hour, description, saveBtn);
     };
 };
 
 timeblockCreation();
-        
-    // store inputs in local storage
-    // clear calendar
+
+$('button').click(function () {
+    event.preventDefault();
+    var currentID = $(this).attr('id')
+    var toStore = $(('#textarea' + currentID)).val();
+    localStorage.setItem(('textarea' + currentID), toStore);
+});
+
+$('textarea').keyup(function () {
+    event.preventDefault();
+    var toStore = $(this).val();
+    var thisEvent = $(this).attr('id');
+    localStorage.setItem(thisEvent, toStore);
+});
+
+
+
+
+function findTime() {
+    for (var i = 0; i < hoursArray.length; i++){
+        var realTime = moment().hour(9 + i);
+        if (moment().isBefore(realTime)) {
+            $('#description' + (i + 1)).addClass('future');
+        } else if (moment().isAfter(realTime)) {
+            $('#description' + (i + 1)).addClass('past');
+        } else {
+            $('#description' + (i + 1)).addClass('present');
+        }
+    };
+};
+
+findTime();
+
     
-     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    // DOM Ready Close
+$('clear-contents').click(function () {
+    localStorage.clear();
+    location.reload();
+});
+
+
 });
